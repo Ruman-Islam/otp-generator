efe4fc5033a6e26b485aaa2ea85479853057145c
Ruman-Islam <rumanislam48@gmail.com>
2022-02-13 20:55:35 +0600

    otp generator done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
-console.log('connected');
+// OTP generator
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    if ((pin + '').length === 4) {
+        return pin;
+    } else {
+        return getPin();
+    }
+}
+
+
+// OTP generator button
+document.querySelector('.generate__btn').addEventListener('click', function () {
+    const otp = getPin();
+    const getOtpField = document.querySelector('.getOtp__field');
+    getOtpField.value = otp;
+});
